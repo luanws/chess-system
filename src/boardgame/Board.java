dcacc7dadf819cd09063dc17bab4854633961fbc
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-10 22:42:48 -0300

    Update Board.java

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -51,6 +51,17 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        if (getPiece(position) == null) return null;
+        Piece aux = getPiece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
     public int getRows() {
         return rows;
     }
