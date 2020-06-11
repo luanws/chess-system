1f642cf922d6d34b1deab8c19692d615e71a260e
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-06-10 22:42:52 -0300

    Update ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -13,14 +14,38 @@ public class ChessMatch {
         initialSetup();
     }
 
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validadeSourcePosition(source);
+        Piece capturedPiece = makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
+    private void validadeSourcePosition(Position position) {
+        if (!board.thereIsAPiece(position)) {
+            throw new ChessException("There is piece on source position");
+        }
+    }
+
     private void placePiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
     private void initialSetup() {
-        placePiece('b', 1, new Rook(board, Color.WHITE));
-        placePiece('h', 5, new King(board, Color.BLACK));
-        placePiece('c', 7, new King(board, Color.WHITE));
+        placePiece('c', 1, new Rook(board, Color.WHITE));
+        placePiece('c', 2, new Rook(board, Color.WHITE));
+        placePiece('d', 2, new Rook(board, Color.WHITE));
+        placePiece('e', 2, new Rook(board, Color.WHITE));
+        placePiece('e', 1, new Rook(board, Color.WHITE));
+        placePiece('d', 1, new King(board, Color.WHITE));
+
+        placePiece('c', 7, new Rook(board, Color.BLACK));
+        placePiece('c', 8, new Rook(board, Color.BLACK));
+        placePiece('d', 7, new Rook(board, Color.BLACK));
+        placePiece('e', 7, new Rook(board, Color.BLACK));
+        placePiece('e', 8, new Rook(board, Color.BLACK));
+        placePiece('d', 8, new King(board, Color.BLACK));
     }
 
     public ChessPiece[][] getPieces() {
